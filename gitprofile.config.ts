8c5a66fe51a626ecbeb54aad0bdb575ddf0517e1
niaralston <nia.ralston@gmail.com>
2025-11-18 19:13:51 -0500

    Change image URLs to relative paths
    Updated image URLs to use relative paths instead of absolute URLs.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -43,7 +43,7 @@ const CONFIG = {
           description:
             'Led design and manufacturing of race car suspension and steering system, reducing weight by 3%. Employed Finite Element Analysis (FEA) and iterative testing to optimize design. Created technical drawings with proper tolerancing and manufactured parts using CNC equipment.',
           imageUrl:
-            'https://github.com/niaralston/niaralston.github.io/blob/main/public/FormulaCollage.png',
+            '/FormulaCollage.png',
           link: 'https://github.com/niaralston',
         },
         {
@@ -51,7 +51,7 @@ const CONFIG = {
           description:
             'Developed control algorithms applying inverse kinematics and path planning, enabling robotic arms to play chess. Refined algorithm to accommodate various arm designs. Implemented circuitry for real-time visualization of chess piece positions.',
           imageUrl:
-            'https://github.com/niaralston/niaralston.github.io/blob/main/public/RobotPlayingChess.JPG',
+            '/RobotPlayingChess.JPG',
           link: 'https://github.com/niaralston',
         },
       ],
