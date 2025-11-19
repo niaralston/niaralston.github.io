2315715a86fd5c372d6cb40cbcfc60edc7f6ef64
niaralston <nia.ralston@gmail.com>
2025-11-19 10:23:23 -0500

    Change mask to rounded-lg for image container to make image wider

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -99,7 +99,7 @@ const ExternalProjectCard = ({
                   </h2>
                   {item.imageUrl && (
                     <div className="w-full opacity-90">
-                      <div className="w-full h-48 mask mask-squircle">
+                      <div className="w-full h-48 rounded-lg overflow-hidden">
                         <LazyImage
                           src={item.imageUrl}
                           alt={'thumbnail'}
