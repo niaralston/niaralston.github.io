ebbc3eecd5dc617669143239ddf1d8d9eda4c54d
Nia Ralston <nia.ralston@gmail.com>
2025-11-19 22:43:45 -0500

    Fixed youtube link

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -206,7 +206,7 @@ publications: [
     imageUrl: 'https://raw.githubusercontent.com/niaralston/niaralston.github.io/main/public/IROSPresentation.png',
     imageUrl2: 'https://raw.githubusercontent.com/niaralston/niaralston.github.io/main/public/IROSPosterPicture.png',
     videoUrl: 'https://raw.githubusercontent.com/niaralston/niaralston.github.io/main/public/SoftWalkerTrimmed.mp4',
-    videoUrl2: 'https://youtu.be/RoTWsi8_IXY',
+    videoUrl2: 'https://www.youtube.com/embed/RoTWsi8_IXY',
   },
   {
     title: 'Learn how Baymax is coming to real life',
